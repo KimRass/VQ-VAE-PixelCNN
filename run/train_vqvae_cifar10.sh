92254f5cd748bed4c9a8606dbaef22d1fd61e11f
KimRass <rmx1000@yonsei.ac.kr>
2024-02-13 21:48:17 +0900

    Using Colab

--- run/train_vqvae_cifar10.sh
+++ run/train_vqvae_cifar10.sh
@@ -5,11 +5,13 @@ source set_pythonpath.sh
 
 python3 ../train_vqvae.py\
     --dataset="cifar10"\
-    --data_dir="/Users/jongbeomkim/Documents/datasets/cifar-10-batches-py"\
-    --save_dir="/Users/jongbeomkim/Documents/vqvae/cifar10/"\
-    --n_cpus=7\
+    --data_dir="/Users/jongbeomkim/Documents/datasets/"\
+    --save_dir="/Users/jongbeomkim/Documents/vqvae/vqvae-cifar10/"\
     --n_epochs=300\
     --batch_size=128\
-    --n_embeds=128\
-    --hidden_dim=256\
-    --n_pixelcnn_res_blocks=2\
+    --n_cpus=7\
+    --lr=0.0002\
+    --n_embeds=32\
+    --hidden_dim=128\
+    --n_pixelcnn_res_blocks=3\
+    --n_pixelcnn_conv_blocks=2\
